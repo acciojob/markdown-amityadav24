8e453a84fb0b823461c358117113e185641b673e
Amit Yadav <103848489+amityadav24@users.noreply.github.com>
2025-07-04 13:19:32 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,9 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import MarkdownEditor from "./MarkdownEditor";
 import "./styles.css";
 
 function App() {
   const [markdownText, setMarkdownText] = useState("");
-
-  useEffect(() => {
-  }, [markdownText]);
 
   return (
     <div className="app">
