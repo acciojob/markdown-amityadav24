c9f2a06a645fcf62cbd0798a71d2e1de7bc4be33
Amit Yadav <103848489+amityadav24@users.noreply.github.com>
2025-07-04 13:09:44 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,13 @@
 import React from "react";
 import MarkdownEditor from "./MarkdownEditor";
-import ".styles/App.css";
+import "./styles.css";
 
-function App() {
+const App = () => {
   return (
     <div className="app">
       <MarkdownEditor />
     </div>
   );
-}
+};
 
 export default App;
